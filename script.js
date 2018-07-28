9c2b0c76a287c0634f730e8d4326c053583ba7b6
cody clark <codyclark2119@gmail.com>
2018-07-28 12:02:31 -0500

    final

--- script.js
+++ script.js
@@ -9,6 +9,34 @@ var locationSearch = "";
 var priceChoice = "";
 var theLat = "";
 var theLng = "";
+var userSearches = 0;
+
+var config = {
+    apiKey: "AIzaSyD4vVgvaLDwYkyIgx0xGW7NCGi_M3LB6-w",
+    authDomain: "new-app-dcdb1.firebaseapp.com",
+    databaseURL: "https://new-app-dcdb1.firebaseio.com",
+    projectId: "new-app-dcdb1",
+    storageBucket: "new-app-dcdb1.appspot.com",
+    messagingSenderId: "378608880710"
+  };
+
+firebase.initializeApp(config);
+var database = firebase.database();
+
+database.ref("/searches").on("child_added", function(snapshot) {
+    console.log(snapshot.val());
+    console.log(snapshot.val(userSearches));
+ })
+ 
+ var storeInputs = function(event) {
+    event.preventDefault();
+ 
+    userSearches = userSearches.val().trim();
+    
+    database.ref("/searches").push({
+        userSearches: userSearches,
+    });
+ }
 
 
 $(".dropdown-item").click(function (event) {
@@ -30,10 +58,12 @@ $("#submit").click(function (event) {
         method: "GET"
     })
         .then(function (response) {
+            console.log(response);
             var theCall = response.data;
             for (x = 0; x < response.response.venues.length; x++) {
                 var theList = $("<button>");
                 theList.addClass("userchoices");
+                theList.addClass("hvr-underline-from-center");
                 theList.attr("value", response.response.venues[x].id);
                 $(theList).text(response.response.venues[x].name);
                 $("#filterOutput").append(theList);
@@ -42,46 +72,50 @@ $("#submit").click(function (event) {
 
 $(".userchoices").click(function (event) {
     event.preventDefault();
+
     var urlTwo = "https://api.foursquare.com/v2/venues/" + this.value + "?&client_id=" + clientId + "&client_secret=" + clientSecret + "&v=20182107";
-    console.log(event);
     $.ajax({
         url: urlTwo,
         method: "GET"
     })
         .then(function (response) {
-            console.log(response);
             var resultsTwo = response.data;
-
+            userSearches++;
             $("#info").html("");
+            $("#menulink").html("");
+            $("#site").html("");
+            $("#name").html("");
+            $("#location").html("")
 
             var cardDiv = $("<div>");
             var bestImg = $("<img>");
             var hoursInf = $("<p>");
 
-            var name = $("<p>").text("Restaraunt Name: " + response.response.venue.name);
-            var location = $("<p>").text("Location: " + response.response.venue.location.formattedAddress);
+            var name = $("<h5>").text("Restaraunt Name: " + response.response.venue.name);
+            var location = $("<h5>").text("Location: " + response.response.venue.location.formattedAddress);
 
             theLat = response.response.venue.location.lat;
             theLng = response.response.venue.location.lng;
 
             $("#info").append(cardDiv);
-            cardDiv.append(name);
-            cardDiv.append(location);
+            $("#name").append(name);
+            $("#location").append(location);
+
 
             if (response.response.venue.hours) {
                 if (response.response.venue.hours.isOpen === true) {
                     hoursInf.text("Its open! " + response.response.venue.hours.status);
-                    cardDiv.append(hoursInf);
+                    $("#name").append(hoursInf);
                 }
                 else if (response.response.venue.hours.isOpen === false) {
                     hoursInf.text("Its Closed. " + response.response.venue.hours.status);
-                    cardDiv.append(hoursInf);
+                    $("#name").append(hoursInf);
                 }
             }
 
             else {
                 hoursInf.text("No Hours Information Available.");
-                cardDiv.append(hoursInf);
+                $("#name").append(hoursInf);
             }
 
             if (response.response.venue.bestPhoto) {
@@ -100,13 +134,13 @@ $(".userchoices").click(function (event) {
                 menu.text("View Menu")
                 menu.attr("href", response.response.venue.menu.url);
                 menu.attr("target", "_blank");
-                cardDiv.append(menu);
+                $("#menulink").append(menu);
             }
 
             else {
                 var menu = $("<p>");
                 menu.text("NO MENU AVAILABLE");
-                cardDiv.append(menu);
+                $("#menulink").append(menu);
             }
 
             if (response.response.venue.url) {
@@ -114,9 +148,21 @@ $(".userchoices").click(function (event) {
                 venueUrl.attr("href", response.response.venue.url);
                 venueUrl.attr("target", "_blank");
                 venueUrl.text(response.response.venue.url);
-                cardDiv.append(venueUrl);
+                $("#site").prepend(venueUrl);
             }
+            else {
+                $("#site").html("");
+                var venueUrl = $("<p>");
+                $(venueUrl).text("NO WEBSITE AVAILABLE");
+                $("#site").prepend(venueUrl);
+            }
+
             secondAjax();
+
+            database.ref().push({
+                searchLimit: userSearches,
+            });
+
             });
         });
     })
@@ -125,7 +171,6 @@ $(".userchoices").click(function (event) {
 function secondAjax() {
     $('#map').html("");
     var mapQuestLink = "https://www.mapquestapi.com/geocoding/v1/reverse?key=" + key + "&location=" + theLat + "," + theLng + "&outFormat=json&thumbMaps=true";
-    console.log(mapQuestLink);
 
     $.ajax({
         url: mapQuestLink,
@@ -135,7 +180,6 @@ function secondAjax() {
 
             var thirdResponse = response.data;
             console.log(response);
-
             var theMap = $('<img>');
             theMap.addClass('userChoice-map');
             $(theMap).attr("src", response.results[0].locations[0].mapUrl); //check
